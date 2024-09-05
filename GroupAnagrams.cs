2c1df86b0670617a92ddc728b2851097bcc64537
Oğuzhan Ergün <Oguzh4n15@hotmail.com>
2024-09-05 17:04:18 +0300

    GroupAnagrams

---
+++ GroupAnagrams.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolutions
+{
+	public class Solution
+	{
+		public Dictionary<string, IList<string>> anagramGroupsMap = new Dictionary<string, IList<string>>();
+		private IList<IList<String>> anagramGroupsList = new List<IList<String>>();
+
+
+		public IList<IList<string>> GroupAnagrams(string[] strs)
+		{
+			foreach (string str in strs)
+			{
+				string orderedstr = sortString(str);
+				if (anagramGroupsMap.TryGetValue(orderedstr, out var list))
+				{
+					list.Add(str);
+				}
+				else
+				{
+					IList<string> a = new List<string>
+					{
+						str
+					};
+					anagramGroupsMap.Add(orderedstr, a);
+				}
+			}
+
+			foreach (var str in anagramGroupsMap) 
+			{
+				anagramGroupsList.Add(str.Value);
+			}
+			return anagramGroupsList;
+		}
+
+
+		static string sortString(String str)
+		{
+			string orderedWord = "";
+			char[] arr = str.ToCharArray();
+			Array.Sort(arr);
+			foreach (char c in arr)
+			{
+				orderedWord += c;
+			}
+			return orderedWord;
+		}
+
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeSolutions
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+
+			Solution solution = new Solution();
+
+			solution.GroupAnagrams(strs);
+		}
+	}
+}
